886a5abbdf132124637ef0ccea0c91c2ec8883a2
Tyler Hudson <tylerhudson96@gmail.com>
2022-12-07 21:57:39 -0500

    Product model v1

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,3 +1,14 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.schema({});
+const ProductSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    inStock: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
