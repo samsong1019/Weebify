b11f9bdb96861d4dbb5806474cbc3e12e921d951
kariukit <nyawirakariukit@gmail.com>
2022-12-12 19:41:01 -0500

    added seeding data and created correct mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,11 +4,14 @@ dotenv.config();
 
 // Setting up connection to Mongo Atlas DB
 const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//const connectDB = async () => {
+ // const conn = await mongoose.connect(process.env.MONGO_URI);
 
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+ // console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+//};
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-};
-
-module.exports = connectDB;
+module.exports = mongoose.connection;
